0369437c741b6e9f8eb775ffb52b91ed25b67647
Tran Hue Chi <tranhuechi1995@gmail.com>
2020-07-12 15:28:24 +0800

    Clean up

--- my-web-app/next.config.js
+++
@@ -1,8 +0,0 @@
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-const withFonts = require('next-fonts')
-const withImages = require('next-images')
-
-module.exports = withImages(withFonts(withCSS(withSass({
-    cssModules: true
-}))));
