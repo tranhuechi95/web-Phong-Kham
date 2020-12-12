42c0e16d401585ea73559bc9d6f5f9e5c6990560
Le Minh Phuc <leminhphuc10t1@gmail.com>
2020-12-12 16:59:17 +0800

    pages: add an empty admin page

---
+++ pages/admin.js
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+
+const Admin = () => {
+    return (
+        <Layout title="Admin">
+            <div className="row">
+                <div className="col-md-1"></div>
+                <div className="col-md-10">
+                    Đây là trang admin trống
+                </div>
+                <div className="col-md-1"></div>
+            </div>
+        </Layout>
+    )
+};
+
+export default Admin;
