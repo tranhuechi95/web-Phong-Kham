a9c437394b8180866d437458b7743810646d1ba7
Le Minh Phuc <leminhphuc10t1@gmail.com>
2021-02-01 23:25:04 +0800

    Update styles for EditArticle file

--- components/EditArticle.js
+++ components/EditArticle.js
@@ -4,7 +4,7 @@ const EditArticle = ({article}) => {
     return (
         <div className="postSmView">
             <Link href={`/edit/${article.RouteName}?year=${article.CreatedDatetime.getFullYear()}`}>
-                <h4>{article.ArticleName}</h4>
+                <h5>- {article.ArticleName}</h5>
             </Link>
             {/* <Link href={{ pathname: `/edit/${article.RouteName}`, query: { year: article.CreatedDatetime.getFullYear() }}}>
                 <h4>{article.ArticleName}</h4>
