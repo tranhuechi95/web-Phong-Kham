a66c730d3d858cf877cfc248e48c829b8e51a5bb
Le Minh Phuc <leminhphuc10t1@gmail.com>
2021-02-06 14:46:22 +0800

    minor clean up

--- components/EditArticle.js
+++ components/EditArticle.js
@@ -6,9 +6,6 @@ const EditArticle = ({article}) => {
             <Link href={`/edit/${article.RouteName}?year=${article.CreatedDatetime.getFullYear()}`}>
                 <h5>- {article.ArticleName}</h5>
             </Link>
-            {/* <Link href={{ pathname: `/edit/${article.RouteName}`, query: { year: article.CreatedDatetime.getFullYear() }}}>
-                <h4>{article.ArticleName}</h4>
-            </Link> */}
             <small>{article.CreatedDatetime.toLocaleDateString('en-US', {timeZone: 'Asia/Bangkok'})}</small>
         </div>
     );
